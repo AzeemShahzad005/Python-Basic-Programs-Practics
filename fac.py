153246f0e698cd1b33d64c33091a6229f8eaf37e
Azeem Shahzad <49363483+AzeemShahzad005@users.noreply.github.com>
2020-03-05 12:10:53 +0500

    Update fac.py

--- fac.py
+++ fac.py
@@ -1,1 +1,12 @@
-#
+#find factorail of any number using python (if else) conditions 
+num = int(input("Enter the Number: "))
+factorial = 1
+if num < 0:
+    print("Factorial can not be found for -ve numbers")
+elif num == 0:
+    print("Factorial of 0 is 1")
+else:
+      for i in range(1, num + 1):
+        factorial = factorial * i
+        
+print("Factorial of ", num, "is: ", factorial)
